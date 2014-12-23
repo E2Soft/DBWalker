c0c29f3cafd3c97b6fcc6fec9012bc6c3463647a
zeljko.bal <zeljko.bal@gmail.com>
2014-12-23 23:50:50 +0100

    - MainForm refactoring

--- DBWalker/src/walker/gui/form/MainForm.java
+++ DBWalker/src/walker/gui/form/MainForm.java
@@ -3,12 +3,10 @@ package walker.gui.form;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -31,10 +29,10 @@ public class MainForm extends JFrame implements Observer{
 	private WorkspaceModel workspaceModel;
     private WorkspaceTree workspaceTree;
 	private ChildrenTablePanel childrenTablePanel;
-    private TablePanel tablePanel;
-    private JSplitPane Hspliter;
-    private JSplitPane Vspliter;
-    private double hDeviderLocation = 0.5;
+    private TablePanel centralTablePanel;
+    private JSplitPane horizontalSpliter;
+    private JSplitPane verticalSpliter;
+    private double horizontalDeviderLocation = 0.5;
     
 	public MainForm(Controller controller, TableData tableData) {
 		initMainFrame();
@@ -72,35 +70,28 @@ public class MainForm extends JFrame implements Observer{
 	
 	private void initMainLayout(Controller controller, TableData tableData){
 		
-		JPanel tree = new JPanel();
-		tree.setOpaque(true);
-		tree.setBackground(Color.white);
-
-		JPanel parrentPAnel = new JPanel();
-		parrentPAnel.setOpaque(true);
-		parrentPAnel.setBackground(Color.red);
-		
-		JPanel childsPanel = new JPanel();
-		childsPanel.setOpaque(true);
-		childsPanel.setBackground(Color.green);
-		
 		childrenTablePanel = new ChildrenTablePanel(controller, tableData);
-		tablePanel = new TablePanel(tableData);
-		JScrollPane HscrollChild = new JScrollPane(childrenTablePanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		HscrollChild.setMinimumSize(new Dimension(250, 250));
-		JScrollPane HscrollParrent = new JScrollPane(tablePanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		centralTablePanel = new TablePanel(tableData);
 		
-		Hspliter = new JSplitPane(JSplitPane.VERTICAL_SPLIT, HscrollParrent, HscrollChild);
-		Hspliter.setDividerLocation(hDeviderLocation);
-		Hspliter.setOneTouchExpandable(true);
+		JScrollPane childrenScroll = new JScrollPane(childrenTablePanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		childrenScroll.setMinimumSize(new Dimension(250, 250));
 		
-		JScrollPane Vscroll = new JScrollPane(tree, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		tree.setLayout(new BorderLayout(0, 0));
+		JScrollPane centralScroll = new JScrollPane(centralTablePanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		
-		//ja
-		//stablo = new JTree();
+		horizontalSpliter = new JSplitPane(JSplitPane.VERTICAL_SPLIT, centralScroll, childrenScroll);
+		horizontalSpliter.setDividerLocation(horizontalDeviderLocation);
+		horizontalSpliter.setOneTouchExpandable(true);
 		
-		tree.add(workspaceTree);//dodaj stablo na panel
+		//tree
+		
+		JPanel treePanel = new JPanel();
+		treePanel.setOpaque(true);
+		treePanel.setBackground(Color.white);
+		
+		JScrollPane treeScroll = new JScrollPane(treePanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		treePanel.setLayout(new BorderLayout(0, 0));
+		
+		treePanel.add(workspaceTree);//dodaj stablo na panel
 		
 		//test part
 		Project project1 = new Project("project1");
@@ -122,58 +113,18 @@ public class MainForm extends JFrame implements Observer{
 		workspaceTree.addProject(project1);
 		workspaceTree.addProject(project2);
 		
-		
-		
-		//end ja
-		
-		
 		JPanel leftPanel = new JPanel(new BorderLayout());
 		leftPanel.setMinimumSize(new Dimension(250, 250));
 		
-		GridLayout grid = new GridLayout(3, 4);
-		grid.setVgap(20);
-		grid.setHgap(20);
-		JPanel jostick = new JPanel(grid);
-		jostick.setOpaque(true);
-		jostick.setBackground(Color.YELLOW);
-		jostick.setPreferredSize(new Dimension(150, 150));
+		leftPanel.add(treeScroll, BorderLayout.CENTER);
 		
-		JButton W = new JButton("W");
-		JButton A = new JButton("A");
-		JButton S = new JButton("S");
-		JButton D = new JButton("D");
+		//end tree
 		
-		JPanel mid = new JPanel();
-		mid.setOpaque(false);
-		JPanel mid2 = new JPanel();
-		mid2.setOpaque(false);
-		JPanel mid3 = new JPanel();
-		mid3.setOpaque(false);
-		JPanel mid4 = new JPanel();
-		mid4.setOpaque(false);
-		JPanel mid5 = new JPanel();
-		mid5.setOpaque(false);
-		JPanel mid6 = new JPanel();
-		mid6.setOpaque(false);
+		verticalSpliter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, horizontalSpliter);
+		verticalSpliter.setDividerLocation(0.6);
+		verticalSpliter.setOneTouchExpandable(true);
 		
-		jostick.add(mid);
-		jostick.add(W);
-		jostick.add(mid2);
-		jostick.add(A);
-		jostick.add(mid3);
-		jostick.add(S);
-		jostick.add(mid4);
-		jostick.add(D);
-		jostick.add(mid5);
-		
-		leftPanel.add(Vscroll, BorderLayout.CENTER);
-	//	leftPanel.add(jostick, BorderLayout.SOUTH);
-		
-		Vspliter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, Hspliter);
-		Vspliter.setDividerLocation(250);
-		Vspliter.setOneTouchExpandable(true);
-		
-		getContentPane().add(Vspliter);
+		getContentPane().add(verticalSpliter);
 	}
 	
 	private void positionAndShow(){
@@ -194,19 +145,19 @@ public class MainForm extends JFrame implements Observer{
 				// ako nema dece sakri donji panel
 				if(appState.getCurrentTable().getChildren().isEmpty())
 				{
-					Vspliter.setRightComponent(tablePanel);
+					verticalSpliter.setRightComponent(centralTablePanel);
 					
 					// zapamti lokaciju devidera
-					hDeviderLocation = Hspliter.getDividerLocation();
+					horizontalDeviderLocation = horizontalSpliter.getDividerLocation();
 				}
 				else
 				{
-					Vspliter.setRightComponent(Hspliter);
+					verticalSpliter.setRightComponent(horizontalSpliter);
 					// vrati devider na prethodnu lokaciju
-					Hspliter.setDividerLocation(hDeviderLocation);
+					horizontalSpliter.setDividerLocation(horizontalDeviderLocation);
 				}
 				
-				tablePanel.updateData(appState.getCurrentTable());
+				centralTablePanel.updateData(appState.getCurrentTable());
 				childrenTablePanel.update(appState.getCurrentTable());
 			}
 			else if(AppState.SCHEMA_MODEL_CHANGED.equals(arg))
