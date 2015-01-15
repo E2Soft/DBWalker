fbbca6aed00a3025e2f3a6ec6df372f00178d5cb
zeljko.bal <zeljko.bal@gmail.com>
2015-01-15 20:35:33 +0100

    - dodato biranje properties fajla sa parametrima za konekciju na bazu u LoadSchemaAction (nakon sto se odabere xml fajl)

--- DBWalker/src/walker/controller/LoadSchemaAction.java
+++ DBWalker/src/walker/controller/LoadSchemaAction.java
@@ -3,7 +3,10 @@ package walker.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -12,40 +15,80 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
+import walker.db.DBConnection;
 import walker.engine.WalkEngine;
-import walker.main.AppState;
-
 import walker.engine.model.Package;
+import walker.main.AppState;
 
 
 
 public class LoadSchemaAction implements ActionListener
 {
 	private AppState appState;
-	private File selektovaniFajl;//TODO ovo izbaci visak je :D !
 
 	public LoadSchemaAction(AppState appState)
 	{
 		this.appState = appState;
 	}
 
-	public File getSelectedXmlFile(){
-		return selektovaniFajl;
-	}
-	
 	@Override
 	public void actionPerformed(ActionEvent e)
+	{
+		try
+		{
+			loadXml();
+			loadDBLoginData();
+		} 
+		catch (Exception e1)
+		{}
+	}
+
+	private void loadXml() throws Exception
 	{
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileFilter(new FileNameExtensionFilter("Phisical Data Model (*.pdm)", "pdm"));
 		fileChooser.setFileFilter(new FileNameExtensionFilter("Phisical Data Model (*.xml)", "xml"));
-		
 		int returnVal = fileChooser.showOpenDialog(null);
-		System.out.println(returnVal);
 
 		if (returnVal == JFileChooser.APPROVE_OPTION)
 		{
-			selektovaniFajl = fileChooser.getSelectedFile();
+			File selektovaniFajl = fileChooser.getSelectedFile();
+
+			if(!selektovaniFajl.canRead())
+			{
+				JOptionPane.showMessageDialog(null, "File is not readable.", "File error", JOptionPane.ERROR_MESSAGE);
+			}
+
+			try
+			{
+				Package schemaModel = WalkEngine.getPackage(selektovaniFajl.getPath());
+
+				appState.setSchemaModel(schemaModel);
+			}
+			catch (ParserConfigurationException | SAXException ex)
+			{
+				JOptionPane.showMessageDialog(null, "Error occured while parsing the file.\n"+ex.getMessage(), "Parsing error", JOptionPane.ERROR_MESSAGE);
+				throw ex;
+			}
+			catch(IOException ex)
+			{
+				JOptionPane.showMessageDialog(null, "Error occured while reading the file.\n"+ex.getMessage(), "File error", JOptionPane.ERROR_MESSAGE);
+				throw ex;
+			}
+		}
+	}
+
+	private void loadDBLoginData() throws Exception
+	{
+		Properties props = new Properties();
+
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new FileNameExtensionFilter("Database login properties (*.properties)", "properties"));
+		int returnVal = fileChooser.showOpenDialog(null);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			File selektovaniFajl = fileChooser.getSelectedFile();
 			
 			if(!selektovaniFajl.canRead())
 			{
@@ -54,18 +97,30 @@ public class LoadSchemaAction implements ActionListener
 			
 			try
 			{
-				Package schemaModel = WalkEngine.getPackage(selektovaniFajl.getPath());
+				props.load(new FileInputStream(selektovaniFajl));
 				
-				appState.setSchemaModel(schemaModel);
-			}
-			catch (ParserConfigurationException | SAXException ex)
+				String driver = props.getProperty("driver"); //Ime parametara
+				String url = props.getProperty("url");
+				String username = props.getProperty("username");  
+				String password = props.getProperty("password");
+				
+				DBConnection.open(driver, url, username, password);
+			} 
+			catch (IOException ex)
 			{
-				JOptionPane.showMessageDialog(null, "Error occured while parsing the file.\n"+ex.getMessage(), "Parsing error", JOptionPane.ERROR_MESSAGE);
-			}
-			catch(IOException ex)
+				JOptionPane.showMessageDialog(null, "Error occured while opening the file.\n"+ex.getMessage(), "Properties file error", JOptionPane.ERROR_MESSAGE);
+				throw ex;
+			} catch (ClassNotFoundException ex)
 			{
-				JOptionPane.showMessageDialog(null, "Error occured while reading the file.\n"+ex.getMessage(), "File error", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Unsuported database driver.", "Properties file error", JOptionPane.ERROR_MESSAGE);
+				throw ex;
+			} catch (SQLException ex)
+			{
+				JOptionPane.showMessageDialog(null, "Error occured while opening the database.\n"+ex.getMessage(), "Properties file error", JOptionPane.ERROR_MESSAGE);
+				throw ex;
 			}
 		}
 	}
 }
+
+
--- DBWalker/src/walker/db/DBConnection.java
+++ DBWalker/src/walker/db/DBConnection.java
@@ -20,14 +20,26 @@ public class DBConnection {
 		return conn;
 	}
 	
+	/**
+	 * ucitaj iz default properties fajla
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	public static void open() throws ClassNotFoundException, SQLException {
 		if (conn != null)
 			return;
 		ResourceBundle bundle = PropertyResourceBundle.getBundle("DBConnection"); //ime fajla
-		String driver = bundle.getString("microsoftDriver"); //Ime parametara
-		String url = bundle.getString("microsoftUrl");
+		String driver = bundle.getString("driver"); //Ime parametara
+		String url = bundle.getString("url");
 		String username = bundle.getString("username");  
 		String password = bundle.getString("password");
+		
+		open(driver, url, username, password);
+	}
+	
+	public static void open(String driver, String url, String username, String password) throws ClassNotFoundException, SQLException 
+	{
+		close(); // if already open close it
 		
 		Class.forName(driver); //Registrovanje drajvera
 		conn = DriverManager.getConnection(url, username, password);
